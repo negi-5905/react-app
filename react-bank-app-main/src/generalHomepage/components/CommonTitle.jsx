d18a6ca9793b3bfcdf31a1ac3096011f01153633
negi-5905 <prathamnegi5905@gmail.com>
2025-04-11 22:43:36 +0530

    Delete react-bank-app-main directory

--- react-bank-app-main/src/generalHomepage/components/CommonTitle.jsx
+++
@@ -1,9 +0,0 @@
-const CommonTitle = ({ title }) => {
-  return (
-    <h1 className="text-3xl ss:text-4xl md:text-5xl leading-normal sm:leading-relaxed md:leading-relaxed font-semibold">
-      {title}
-    </h1>
-  );
-};
-
-export default CommonTitle;
